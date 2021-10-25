1d39be3786f94d8e38c890ef9ad629bb1d4d9778
Daivi <dpatel404@gatech.edu>
2021-10-25 17:16:10 -0400

    personal access token

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas
 import re
 import numpy as np
+import nltk
+
 
 # Daivi's personal access token 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
 
@@ -13,10 +15,20 @@ def remove_at(text):
     return text
 
 
+# def format_sentence(sent):
+#     return ({word: True for word in nltk.word_tokenize(sent)})
+
+def tag_pos(sent):
+    tokenized = nltk.word_tokenize(sent)
+    pos_tags = nltk.pos_tag(tokenized)
+    return pos_tags
+
+
 df = pandas.read_csv('data/train_data.csv', encoding='latin-1')
 df.columns = ['Polarity', 'ID', 'Date', 'Query', 'User', 'Text']
 print(df)
 df['Text'] = df['Text'].apply(remove_at)
 print(df)
+df['POS'] = df['Text'][0:3].apply(tag_pos)
+print(df['POS'].iloc[0])
 
-#add code
