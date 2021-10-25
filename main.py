1ca88c38fdcbbf2652f50bfcf0a0be3d1910fc30
Daivi <dpatel404@gatech.edu>
2021-10-25 13:32:30 -0400

    add data

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-#test
+import pandas
+import re
+import numpy as np
+
+
+def remove_at(text):
+    # A username can only contain alphanumeric characters (letters A-Z, numbers 0-9) with the exception of underscores,
+    # as noted above. Check to make sure your desired username doesn't contain any symbols, dashes, or spaces.
+    pattern = r'@([A-Za-z0-9_])*'
+    # Replace all occurrences of @username with an empty string
+    text = re.sub(pattern, '', text)
+    return text
+
+
+df = pandas.read_csv('data/train_data.csv', encoding='latin-1')
+df.columns = ['Polarity', 'ID', 'Date', 'Query', 'User', 'Text']
+print(df)
+df['Text'] = df['Text'].apply(remove_at)
+print(df)
