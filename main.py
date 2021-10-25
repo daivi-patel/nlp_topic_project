ed43f0449da9db98672adc48e4e3cdab9ace95a3
Daivi <dpatel404@gatech.edu>
2021-10-25 19:22:30 -0400

    nouns

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ import numpy as np
 import nltk
 
 
-# Daivi's personal access token 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
-
 def remove_at(text):
     # A username can only contain alphanumeric characters (letters A-Z, numbers 0-9) with the exception of underscores,
     # as noted above. Check to make sure your desired username doesn't contain any symbols, dashes, or spaces.
@@ -15,20 +13,45 @@ def remove_at(text):
     return text
 
 
-# def format_sentence(sent):
-#     return ({word: True for word in nltk.word_tokenize(sent)})
-
 def tag_pos(sent):
     tokenized = nltk.word_tokenize(sent)
     pos_tags = nltk.pos_tag(tokenized)
     return pos_tags
 
 
-df = pandas.read_csv('data/train_data.csv', encoding='latin-1')
-df.columns = ['Polarity', 'ID', 'Date', 'Query', 'User', 'Text']
-print(df)
-df['Text'] = df['Text'].apply(remove_at)
-print(df)
-df['POS'] = df['Text'][0:3].apply(tag_pos)
-print(df['POS'].iloc[0])
+def nouns(list):
+    return [i[0] for i in list if i[1] == "NN" or i[1] == "NNS" or i[1] == "NNP" or i[1] == "NNPS"]
 
+
+def f(x):
+    if x['Topic'] in x['Nouns']:
+        return True
+    else:
+        return False
+
+
+df = pandas.read_csv('data/test_data.csv', encoding='latin-1')
+# df.columns = ['Polarity', 'ID', 'Date', 'Query', 'User', 'Text']
+df.columns = ['Polarity', 'ID', 'Date', 'Topic', 'User', 'Text']
+# print(df)
+df['Text'] = df['Text'].apply(remove_at)
+# print(df)
+df['POS'] = df['Text'].apply(tag_pos)
+df['Nouns'] = df['POS'].apply(nouns)
+
+
+def find_value_column(r):
+    new_list = []
+    for noun in r.Nouns:
+        new_list.append(noun.lower().strip())
+
+    return r.Topic.lower().strip() in new_list
+
+for row in df.loc[df.Nouns.isnull(), 'Nouns'].index:
+    df.at[row, 'Nouns'] = []
+
+df['Acc'] = df.apply(find_value_column, axis=1)
+print(df)
+# print(df['Acc'].value_counts.True)
+# print(df['Acc'].describe())
+print(df.groupby('Acc').count())
